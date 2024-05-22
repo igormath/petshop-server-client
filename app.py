a54e3a9aefadc075cdddca3a7cbd368dca272ce6
igormath <igorxt9@gmail.com>
2024-05-22 01:34:27 -0300

    Update app.py with various new routes

--- app.py
+++ app.py
@@ -2,10 +2,43 @@ from flask import Flask, request, render_template, jsonify
 from data import produtos
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route('/main')
+def index():
+    return render_template('index.html')
+
+@app.route('/')
+def crud():
+    return render_template('crud.html')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+@app.route('/cadastrar')
+def cadastrar():
+    return render_template('cadastrar.html')
+
+@app.route('/remover')
+def remover():
+    return render_template('remover.html')
+
+@app.route('/atualizar')
+def atualizar():
+    return render_template('atualizar.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 @app.route('/produtos', methods=['GET'])
 def read():
-    return produtos
+    nome = request.args.get('name')
+    if nome:
+        produtosEncontrados = [produto for produto in produtos if nome.lower() in produto['name'].lower()]
+        return produtosEncontrados, 200
+    return produtos, 200
 
 @app.route('/produtos', methods=['POST'])
 def create():
